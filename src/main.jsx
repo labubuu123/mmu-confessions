d9196e80ae585358d43728fc6329bc5213ab3b98
labubu <yawjiajun8@gmail.com>
2025-11-15 21:59:03 +0800

    Commit Changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/mmu-confessions/">
       <App />
     </Router>
   </React.StrictMode>,
