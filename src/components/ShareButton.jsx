bece4a5f4863fcebc68916f670ba2683a5091f86
labubu <yawjiajun8@gmail.com>
2025-11-15 20:06:02 +0800

    Commit Changes

--- src/components/ShareButton.jsx
+++ src/components/ShareButton.jsx
@@ -5,7 +5,6 @@ import {
     Copy,
     Check,
     Facebook,
-    Twitter,
     MessageCircle,
     Mail,
     Download
@@ -64,14 +63,8 @@ export default function ShareButton({ post }) {
             case 'facebook':
                 url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(shareUrl)}`
                 break
-            case 'twitter':
-                url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(shareText)}&url=${encodeURIComponent(shareUrl)}`
-                break
             case 'whatsapp':
                 url = `https://wa.me/?text=${encodeURIComponent(shareText + ' ' + shareUrl)}`
-                break
-            case 'telegram':
-                url = `https://t.me/share/url?url=${encodeURIComponent(shareUrl)}&text=${encodeURIComponent(shareText)}`
                 break
             case 'email':
                 url = `mailto:?subject=${encodeURIComponent('MMU Confession')}&body=${encodeURIComponent(shareText + '\n\n' + shareUrl)}`
@@ -209,7 +202,7 @@ export default function ShareButton({ post }) {
                                 </div>
                             </div>
 
-                            <div className="grid grid-cols-3 gap-3 mb-4">
+                            <div className="grid grid-cols-2 gap-3 mb-4">
                                 <SharePlatformButton
                                     icon={<Facebook className="w-5 h-5" />}
                                     label="Facebook"
@@ -217,26 +210,10 @@ export default function ShareButton({ post }) {
                                     onClick={() => handleShare('facebook')}
                                 />
                                 <SharePlatformButton
-                                    icon={<Twitter className="w-5 h-5" />}
-                                    label="Twitter"
-                                    color="bg-sky-500 hover:bg-sky-600"
-                                    onClick={() => handleShare('twitter')}
-                                />
-                                <SharePlatformButton
                                     icon={<MessageCircle className="w-5 h-5" />}
                                     label="WhatsApp"
                                     color="bg-green-600 hover:bg-green-700"
                                     onClick={() => handleShare('whatsapp')}
-                                />
-                                <SharePlatformButton
-                                    icon={
-                                        <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor">
-                                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.64 6.8c-.15 1.58-.8 5.42-1.13 7.19-.14.75-.42 1-.68 1.03-.58.05-1.02-.38-1.58-.75-.88-.58-1.38-.94-2.23-1.5-.99-.65-.35-1.01.22-1.59.15-.15 2.71-2.48 2.76-2.69a.2.2 0 00-.05-.18c-.06-.05-.14-.03-.21-.02-.09.02-1.49.95-4.22 2.79-.4.27-.76.41-1.08.4-.36-.01-1.04-.2-1.55-.37-.63-.2-1.12-.31-1.08-.66.02-.18.27-.36.74-.55 2.92-1.27 4.86-2.11 5.83-2.51 2.78-1.16 3.35-1.36 3.73-1.36.08 0 .27.02.39.12.1.08.13.19.14.27-.01.06.01.24 0 .38z" />
-                                        </svg>
-                                    }
-                                    label="Telegram"
-                                    color="bg-blue-500 hover:bg-blue-600"
-                                    onClick={() => handleShare('telegram')}
                                 />
                                 <SharePlatformButton
                                     icon={<Mail className="w-5 h-5" />}
