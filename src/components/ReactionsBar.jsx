c0d114290474d52f926b63edcfa8e0dcd85de3da
labubu <yawjiajun8@gmail.com>
2025-11-27 23:21:32 +0800

    Commit Changes

--- src/components/ReactionsBar.jsx
+++ src/components/ReactionsBar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { supabase } from '../lib/supabaseClient'
 
-const COMMENT_EMOJIS = [
+const EMOJIS = [
     '👍', '❤️', '😂', '😮', '😢', '🎉', '🤔', '🙏', '👏', '🤯', '😍', '🧐',
     '🔥', '😆', '😎'
 ]
