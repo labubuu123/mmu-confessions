4c071c92dd2d3a1478cbda758f15379f0a4f5a62
labubu <yawjiajun8@gmail.com>
2025-12-02 20:19:39 +0800

    Commit Changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      manifest: {
+        name: 'MMU Confessions',
+        short_name: 'MMU Confess',
+        description: 'Anonymous Student Confessions',
+        theme_color: '#4F46E5',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/favicon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml'
+          },
+        ]
+      }
+    })
+  ],
   base: '/',
   build: {
     outDir: 'dist',
