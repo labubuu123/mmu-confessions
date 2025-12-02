06948c5292fa52005ead2e072f17a938265b61ec
labubu <yawjiajun8@gmail.com>
2025-12-02 18:45:43 +0800

    Commit Changes

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -9,6 +9,7 @@ import PollCreator from './PollCreator';
 import EventCreator from './EventCreator';
 import SeriesManager from './SeriesManager';
 import MoodSelector from './MoodSelector';
+import { useNotifications } from './NotificationSystem';
 
 const MAX_VIDEO_SIZE_MB = 25;
 const MAX_IMAGES = 3;
@@ -88,12 +89,7 @@ export default function PostForm({ onPosted }) {
     const [eventData, setEventData] = useState(null);
     const [showSeriesManager, setShowSeriesManager] = useState(false);
     const [seriesData, setSeriesData] = useState(null);
-
-    const success = (msg) => alert(msg);
-    const error = (msg) => alert(msg);
-    const warning = (msg) => alert(msg);
-    const info = (msg) => console.log(msg);
-
+    const { success, error, warning, info } = useNotifications();
     const [selectedMood, setSelectedMood] = useState(null);
     const [existingSeries, setExistingSeries] = useState([]);
     const [loadingSeries, setLoadingSeries] = useState(false);
