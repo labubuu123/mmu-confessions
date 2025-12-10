a035ced8c84823ef8b36fadae1a6a22e55c5f5cb
labubu <yawjiajun8@gmail.com>
2025-12-10 11:04:56 +0800

    Commit Changes

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'ads.txt'],
+      includeAssets: ['favicon.svg', 'robots.txt', 'ads.txt', 'sitemap.xml'],
       manifest: {
         name: 'MMU Confessions',
         short_name: 'MMU Confess',
