cde622e37640f5a1675b0f7f5de7276c8216fc8b
labubu <yawjiajun8@gmail.com>
2025-12-27 17:42:28 +0800

    Commit Changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u7da3d63fus"
+    "revision": "0.unnplfv46io"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
