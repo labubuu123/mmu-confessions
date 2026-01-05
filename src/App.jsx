e75f95e4d984c730fd902518abd6c7a802e6656c
labubu <yawjiajun8@gmail.com>
2026-01-05 18:06:51 +0800

    Commit Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, lazy, Suspense } from "react";
 import { Routes, Route, useParams, useNavigate, Link } from "react-router-dom";
 import Feed from "./components/Feed";
 import TopConfessions from "./components/TopConfessions";
